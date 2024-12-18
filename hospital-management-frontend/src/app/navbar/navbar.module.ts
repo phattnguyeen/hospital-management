80fbac9a2d4e05da696115b60d84bdb0440e9e97
PhatNguyen3174 <phatnguyen9712@gmail.com>
2024-12-18 12:37:53 +0700

    Added hospital-management-frontend as a regular folder

--- hospital-management-frontend/src/app/navbar/navbar.module.ts
+++ hospital-management-frontend/src/app/navbar/navbar.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderNavComponent } from './header-nav/header-nav.component';
 import { RouterModule } from '@angular/router';
+import { FooterNavComponent } from './footer-nav/footer-nav.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderNavComponent
+    HeaderNavComponent,
+    FooterNavComponent,
+    MainNavComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderNavComponent
+    HeaderNavComponent,
+    FooterNavComponent,
+    MainNavComponent
 
   ]
 })

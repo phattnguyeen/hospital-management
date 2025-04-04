0e2ad363132ccdbd707a5e59a92962b1c9b31f9e
PhatNguyen3174 <phatnguyen9712@gmail.com>
2025-04-04 11:25:35 +0700

    Removed sensitive information and updated .gitignore

--- hospital-management-backend/test2.py
+++ hospital-management-backend/test2.py
@@ -1,15 +1,15 @@
-from twilio.rest import Client
+# from twilio.rest import Client
 
-TWILIO_ACCOUNT_SID = 'AC09bf04efa93ae42365980b3b2361d2df'
-TWILIO_AUTH_TOKEN = '0315a992197dd70559d1e8b15a560ab1'
-TWILIO_PHONE_NUMBER = '+12318880770'  # Replace with your Twilio number
+# TWILIO_ACCOUNT_SID = 'AC09bf04efa93ae42365980b3b2361d2df'
+# TWILIO_AUTH_TOKEN = '0315a992197dd70559d1e8b15a560ab1'
+# TWILIO_PHONE_NUMBER = '+12318880770'  # Replace with your Twilio number
 
-client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+# client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
-message = client.messages.create(
-    body="Test message from Twilio!",
-    from_=TWILIO_PHONE_NUMBER,
-    to="+84798699203"  # Replace with your actual phone number
-)
+# message = client.messages.create(
+#     body="Test message from Twilio!",
+#     from_=TWILIO_PHONE_NUMBER,
+#     to="+84798699203"  # Replace with your actual phone number
+# )
 
-print(f"Message SID: {message.sid}")
+# print(f"Message SID: {message.sid}")
